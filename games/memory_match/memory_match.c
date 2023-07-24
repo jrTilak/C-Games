89a3cea0d42655c10a3153859fe790153fe5dabb
UTSAB PANDEY <pandeyutsab0@gmail.com>
2023-07-24 09:46:08 +0545

    bug removed

--- games/memory_match/memory_match.c
+++ games/memory_match/memory_match.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 int main()
 {
-    int n,i;
-    int randnum[1];
+    int n, i;
+    int num;
     srand(time(NULL));
     printf("welcome to memory match");
     printf("\nround 1");
-    for(i=0;i<1;i++)
-    {
-        randnum[i]=rand();
-         printf("      \n%d",rand());
 
-    }
-    // printf("      \n%d",rand());
+    num = rand();
+    printf("\n%d", num);
     sleep(1);
     system("cls");
     printf("enter the above random number");
-    scanf("%d",&n);
-    if(randnum(i)==n)
+    scanf("%d", &n);
+    if (num == n)
     {
         printf("u got it ");
+    }
+    else
+    {
+        printf("sorry");
     }
 
     return 0;
